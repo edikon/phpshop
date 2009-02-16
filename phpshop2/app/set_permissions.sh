7eaa86bd98b291ad8da17874edd33014c07d1806
edikon <edikon@6862a70d-5b44-0410-9960-4bf35bc44058>
2009-02-16 03:08:09 +0000

    added MIT copyright to index.php files in app and webroot
    updated set_permissions.sh script to use correct path relative to app/ folder

--- phpshop2/app/set_permissions.sh
+++ phpshop2/app/set_permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-chmod -R 777 tmp/
-chmod 777 webroot/img/categories/
-chmod 777 webroot/img/products/
-chmod 777 webroot/img/thumbnails/
-chmod 777 config/
+chmod -R 777 app/tmp/
+chmod 777 app/webroot/img/categories/
+chmod 777 app/webroot/img/products/
+chmod 777 app/webroot/img/thumbnails/
+chmod 777 app/config/
